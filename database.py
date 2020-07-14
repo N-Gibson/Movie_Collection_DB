86d037bb5fd25015c6558e6e4a58ecfc1d7751e9
Noah Gibson <noah.gibson99@gmail.com>
2020-07-13 18:00:10 -0600

    Create database and movie table

---
+++ database.py
@@ -0,0 +1,11 @@
+import mysql.connector
+
+config = {
+  'user': 'root',
+  'password': 'hackerman',
+  'host': 'localhost'
+  'database': 'movie_collection'
+}
+
+db = mysql.connector.connect(**config)
+cursor = db.cursor()
